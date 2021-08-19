770d843ab6561c244aa5822af94ad577b7059217
Aram Zahedi <aram.zahedi@247labs.com>
2021-08-19 15:20:38 +0430

    added: filter tools

---
+++ app/Libraries/Filterable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Libraries;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait Filterable
+{
+    /**
+     * Filter the results
+     *
+     * @param Builder $query
+     * @param QueryFilter $filter
+     * @return Builder
+     */
+    public function scopeFilter(Builder $query, QueryFilter $filter)
+    {
+        return $filter->apply($query);
+    }
+}
