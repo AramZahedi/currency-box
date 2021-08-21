c9e42d11710ce3cdf05f77f31309017e8592bdad
Aram Zahedi <aram.zahedi@247labs.com>
2021-08-22 00:41:53 +0430

    added and commented test for seeding rates

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,31 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Rate;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+//    /** @test */
+//    public function seed_data()
+//    {
+//        $currencies = Rate::currencies();
+//        $count = count($currencies);
+//
+//        for ($i = 0; $i < 300; $i++) {
+//            $data = [];
+//
+//            for ($j = 0; $j < 10000; $j++) {
+//                $currency = $currencies[rand(0, $count - 1)];
+//
+//                $data[] = [
+//                    'price' => rand(),
+//                    'currency' => $currency,
+//                    'created_at' => now()->addSeconds(rand(0, 1000000) * (rand(0, 1) ? -1 : 1))
+//                ];
+//            }
+//
+//            Rate::insert($data);
+//        }
+//    }
 }
