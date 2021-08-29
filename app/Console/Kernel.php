bf45632d7226cb5b3f69db38d8980381d981c383
Aram Zahedi <aram.zahedi@247labs.com>
2021-08-29 15:33:12 +0430

    changed the frequency of the crawler cron job to once per 15 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,14 +20,14 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(
             new CrawlRates()
-        )->everyMinute();
+        )->everyFifteenMinutes();
     }
 
     /**
